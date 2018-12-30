624afbddb9f77da793890cd476662fe3a4f5a9a0
fhsbatista <fernandohsbatista@gmail.com>
2018-12-30 11:32:34 -0200

    Initial commit

---
+++ app/src/main/java/com/fbatista/smsapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.fbatista.smsapp
+
+import android.Manifest
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+
+}
